2262dfa50a584be4a1cd3af09593e337b5db2bbd
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-05-20 21:37:17 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ZaneKit
+
+A description of this package.
---
+++ Sources/ZaneKit/ZaneKit.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Zaid Neurothrone on 2022-05-20.
+//
+
+import Foundation
+
+public struct ZaneKit {
+    public static func configure() {
+        print("Configuring ZaneKit")
+    }
+}
